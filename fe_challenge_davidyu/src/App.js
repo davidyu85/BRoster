d089fdf2d10b916b9089d6816ea31c4c7fb13af9
Chih-Hao <c.yu@garvan.org.au>
2018-07-24 23:57:52 +1000

    Create a basic navigation.

--- fe_challenge_davidyu/src/App.js
+++ fe_challenge_davidyu/src/App.js
@@ -1,14 +1,26 @@
-import React, { Component } from 'react';
+/**
+ * App.js - The main page container.
+ */
 
+import React, { Component } from 'react';
+import { Navigation } from './components/Navigation';
+import { getRequiredData } from './apis';
 
 class App extends Component {
+  componentWillMount() {
+    getRequiredData()
+      .then((response) => {
+        console.log(response.data);
+      });
+  }
+  
   render() {
     return (
       <div>
-        TEST
+        <Navigation />
       </div>
-    );
+    )
   }
-}
+};
 
 export default App;
---
+++ fe_challenge_davidyu/src/components/Navigation.js
@@ -0,0 +1,49 @@
+/**
+ * Nav.js - The navigation sidebar.
+ */
+
+import React from 'react';
+import styled from 'styled-components';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavLink,
+  NavItem
+} from 'reactstrap';
+
+const NavbarStyled = styled(Navbar)`
+  background: #700;
+  
+  a {
+    color:#fff;
+  }
+`;
+
+const NavbarBrandStyle = styled(NavbarBrand)`
+  @media (min-width:600px) {
+    margin-left: 70px;
+  }
+
+  margin-right: 90px !important;
+  font-family: 'Roboto Slab', serif;
+`;
+
+export const Navigation = () => (
+  <NavbarStyled expand="md">
+    <NavbarBrandStyle href="/"><h3>BRoster</h3></NavbarBrandStyle>
+    <NavbarToggler />
+    <Collapse navbar>
+      <Nav navbar>
+        <NavItem>
+          <NavLink href="/">Tabular view</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/">Timeline visualisation</NavLink>
+        </NavItem>
+      </Nav>
+    </Collapse>
+  </NavbarStyled>
+);
